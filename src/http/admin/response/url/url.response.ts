e4cdf6a22c20f8d2a9a49b25125d1a133a6898cb
gabrielfula <gabriel.fulaneto0301@gmail.com>
2025-06-22 10:14:43 -0300

    chore: add date fns

--- src/http/admin/response/url/url.response.ts
+++ src/http/admin/response/url/url.response.ts
@@ -1,10 +1,17 @@
+import { formatDistanceToNow } from "date-fns"
+import { ptBR } from "date-fns/locale"
+
 export class LinkSerializer {
      static serialize(data: any) {
           return {
                uuid: data.uuid,
                original_link: data.original_link,
                new_link: data.new_link,
-               alias: data.alias
+               alias: data.alias,
+               date: formatDistanceToNow(new Date(data.created_at), {
+                    addSuffix: true,
+                    locale: ptBR,
+               }),
           };
      }
 }
